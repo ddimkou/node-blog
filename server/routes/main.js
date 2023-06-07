875d18350f705f80babd7fcae9080692c420d87c
ddimkou <ddimkou@outlook.com>
2023-06-07 13:30:09 +0300

    added mongoDB, added posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../modules/Post");
 
 // Route
-router.get("", (req, res) => {
-  const locals = {
-    title: "nodejs blog",
-    description: "Simple blog created w node, express mongo,db",
-  };
-  res.render("index", { locals });
+router.get("", async (req, res) => {
+  try {
+    const locals = {
+      title: "nodejs blog",
+      description: "Simple blog created with node, express mongo,db",
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAd: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
